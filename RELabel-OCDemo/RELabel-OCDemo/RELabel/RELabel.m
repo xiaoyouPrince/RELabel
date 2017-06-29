64ac29477155ef0d7828a38dbe50bc2ca616666e
xiaoyouPrince <xiaoyouPrince@163.com>
2017-06-29 17:56:23 +0800

    convert to OC

--- RELabel-OCDemo/RELabel-OCDemo/RELabel/RELabel.m
+++ RELabel-OCDemo/RELabel-OCDemo/RELabel/RELabel.m
@@ -42,10 +42,47 @@
     if (self == [super initWithFrame:frame]) {
       
         [self setDefaultValue];
+        
+        [self prepareTextSystem];
     }
     return self;
 }
 
+- (instancetype)initWithCoder:(NSCoder *)aDecoder
+{
+    if (self == [super initWithCoder:aDecoder]) {
+       
+        [self setDefaultValue];
+        
+        [self prepareTextSystem];
+    }
+    return self;
+}
+
+- (void)layoutSubviews
+{
+    [super layoutSubviews];
+    
+    // 设置容器的大小为Label的尺寸
+    self.textContainer.size = self.frame.size;
+}
+
+- (void)drawTextInRect:(CGRect)rect
+{
+    // 背景
+    if (self.selectedRange.length) {
+        
+        UIColor *selectedColor = self.isSelected ? [UIColor colorWithWhite:0.7 alpha:0.2] : [UIColor clearColor];
+  
+        [self.textStorage setAttributes:@{NSBackgroundColorAttributeName : selectedColor} range:self.selectedRange];
+        
+        [self.layoutManager drawBackgroundForGlyphRange:self.selectedRange atPoint:CGPointMake(0, 0)];
+    }
+    
+    // 字形
+    NSRange range = NSMakeRange(0, self.textStorage.length);
+    [self.layoutManager drawGlyphsForGlyphRange:range atPoint:CGPointZero];
+}
 
 /**
     设置默认属性 --- 这些东西直接用懒加载？？
@@ -65,9 +102,12 @@
     _topicRanges = [NSMutableArray new];
     _customRanges = [NSMutableArray new];
     
-    _tapType = TapHandlerTypeNone; // @0 
+    _tapType = TapHandlerTypeNone; // @0
   
 }
+
+
+
 
 
 #pragma mark -- 重写系统属性
@@ -131,15 +171,130 @@
 #pragma mark -- 懒加载私有属性
 
 #pragma mark -- 文字系统和匹配
+// 准备文本系统
 - (void)prepareTextSystem{
     
+    [self prepareText];
+    
+    [self.textStorage addLayoutManager:self.layoutManager];
+    
+    [self.layoutManager addTextContainer:self.textContainer];
+    
+    [self setUserInteractionEnabled:YES];
+    
+    self.textContainer.lineFragmentPadding = 0;
+    
 }
 
+// 准备文本
 - (void)prepareText{
     
+    // 1.准备字符串
     NSAttributedString * attrString = [NSAttributedString new];
+    if (self.attributedText) {
+        attrString = self.attributedText;
+    }else if (self.text)
+    {
+        attrString = [[NSAttributedString alloc] initWithString:self.text];
+    }
+    
+    self.selectedRange = NSMakeRange(0, 0);
+    
+    // 2.准备换行模型
+    NSMutableAttributedString *attrStringM = [self addLineBreakForAttrStr:attrString];
+    
+    [attrStringM addAttribute:NSFontAttributeName value:self.font range:NSMakeRange(0, attrStringM.length)];
+    
+    // 3.textStorage 内容
+    [self.textStorage setAttributedString:attrStringM];
+    
+    // 5.根据用户匹配类型进行对应匹配
+    for (NSNumber *number in self.matchTypes) {
+        
+        TapHandlerType type = TapHandlerTypeNone;
+        if ([number  isEqual: @0]) {
+            type = TapHandlerTypeNone;
+        }else if ([number  isEqual: @1]) {
+            type = TapHandlerTypeUser;
+        }else if ([number  isEqual: @2]) {
+            type = TapHandlerTypeTopic;
+        }else if ([number  isEqual: @3]) {
+            type = TapHandlerTypeLink;
+        }
+        
+        switch (type) {
+            case TapHandlerTypeNone:
+                
+                [self matchCustomString:self.customMatchString]; // 有就有，没有直接就空了
+                
+                break;
+                
+            case TapHandlerTypeUser:
+                
+                [self matchUser];
+
+                break;
+            case TapHandlerTypeTopic:
+                
+                [self matchTopic];
+                
+                break;
+            case TapHandlerTypeLink:
+                
+                [self matchLink];
+                
+                break;
+                
+            default:
+                break;
+        }
+    }
+    
+    [self setNeedsDisplay];
 
 }
+
+- (void)matchUser{
+    
+    
+}
+
+- (void)matchTopic{
+    
+}
+
+- (void)matchLink{
+    
+}
+
+- (void)matchCustomString:(NSString *)customString{
+    
+}
+
+- (NSArray *)getRangesForPattern:(NSString *)pattern
+{
+    
+//    NSRange range = NSMakeRange(0, 0);
+//    
+////    NSArray *a = [NSArray arrayWithObjects:range, nil];
+    
+    NSRegularExpression *regex = [[NSRegularExpression alloc] initWithPattern:pattern options:0 error:nil];
+    
+    return [self getRangesFromResultWithRegex:regex];
+}
+
+- (NSArray *)getLinkRanges{
+    
+    NSDataDetector * detector = [NSDataDetector alloc] initWithTypes:<#(NSTextCheckingTypes)#> error:<#(NSError * _Nullable __autoreleasing * _Nullable)#>
+    
+}
+
+- (NSArray *)getRangesFromResultWithRegex:(NSRegularExpression *)regex
+{
+    
+}
+
+
 
 #pragma mark -- 字符串匹配的封装
 
@@ -147,6 +302,32 @@
 #pragma mark -- 点击交互封装
 
 #pragma mark -- 换行补充
+
+- (NSMutableAttributedString *)addLineBreakForAttrStr:(NSAttributedString *)attrString{
+    
+    NSMutableAttributedString *attrStringM = [[NSMutableAttributedString alloc]initWithAttributedString:attrString];
+    
+    if (attrStringM.length == 0) {
+        return attrStringM;
+    }
+    
+    NSRange range = NSMakeRange(0, 0);
+    NSMutableDictionary * attribute = (NSMutableDictionary *)[attrStringM attributesAtIndex:0 effectiveRange:&range];
+    NSMutableParagraphStyle *paragraphStyle = attribute[NSParagraphStyleAttributeName];
+    
+    if (paragraphStyle != nil) {
+        paragraphStyle.lineBreakMode = NSLineBreakByWordWrapping;
+    }else{
+        paragraphStyle = [NSMutableParagraphStyle new];
+        paragraphStyle.lineBreakMode = NSLineBreakByWordWrapping;
+        attribute[NSParagraphStyleAttributeName] = paragraphStyle;
+        
+        [attrStringM setAttributes:attribute range:range];
+    }
+
+    return attrStringM;
+    
+}
 
 
 
